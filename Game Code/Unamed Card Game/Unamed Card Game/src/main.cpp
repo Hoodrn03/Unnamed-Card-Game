205030695168876bff41d19c95db4fecc56e406e
Ryan <Hoodrn03@gmail.com>
2019-08-13 12:56:37 +0100

    Linked TGUI and SFML
    I have linked both the SFMl and TGUI libraries into the game project. I have also added in the minimal code required to run the game.

--- Game Code/Unamed Card Game/Unamed Card Game/src/main.cpp
+++ Game Code/Unamed Card Game/Unamed Card Game/src/main.cpp
@@ -1,9 +1,33 @@
-/*! \file main.cpp 
-*		This is the entry point for the project.
+/*! \file main.cpp
+*		This is the entry point for the project. 
 */
 
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	return 0;
+	sf::RenderWindow l_Window(sf::VideoMode(800, 800), "Unamed Card Game"); 
+
+	while (l_Window.isOpen())
+	{
+		sf::Event l_Event;
+
+		// Events Here. 
+
+		while (l_Window.pollEvent(l_Event))
+		{
+			if (l_Event.type == sf::Event::Closed)
+			{
+				return 1;
+			}
+		}
+
+		l_Window.clear();
+
+		// Draw Here. 
+
+		l_Window.display();
+	}
+
+	return 0; 
 }
