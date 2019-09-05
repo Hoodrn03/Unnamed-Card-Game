560e577302291d6c32607087e6d285b524e668a3
Ryan <Hoodrn03@gmail.com>
2019-09-05 14:44:46 +0100

    Created Card Slots
    They are not evenly placed on the screen but they are however there. They will act as place holders for the moment and allow for progression with the game.

--- Game Code/Unamed Card Game/Unamed Card Game/src/GameBoard.cpp
+++ Game Code/Unamed Card Game/Unamed Card Game/src/GameBoard.cpp
@@ -41,21 +41,68 @@ void GameBoard::m_CreateGameBoard(sf::RenderWindow& window)
 	// This should create the game board using the size of the current game window. To ensure the box is within the center of the screen then you will
 	// need to work out what will get the width to 1 then  half what you would need to add to get it there. E.g. 0.5 + 0.5 = 1 so the offset will be 2.5; 
 
+	// Create main game board.
 
 	/*! \var The height and width for the game board. */
 	float l_fWidth = 0.7f, l_fHeight = 0.5f;
 
 	/*! \var The offset for the game board to place it within the center. */
 	float l_fWidthOffSet = 0.15f, l_fHeightOffset = 0.25f; 
-
-	// Create main game board.
-
+	
 	m_GameBoard.setSize(sf::Vector2f(window.getSize().x * l_fWidth, window.getSize().y * l_fHeight));
 
 	m_GameBoard.setPosition(sf::Vector2f(window.getSize().x * l_fWidthOffSet, window.getSize().y * l_fHeightOffset));
 
 	// Create card slots for game board. 
 
+	const int l_iNumberOfSlots = 3;
+
+	float l_fCardSlotWidth = m_GameBoard.getGlobalBounds().width * 0.2f, 
+		l_fCardSlotHeight = m_GameBoard.getGlobalBounds().height * 0.2f;
+
+	float l_fCardSlotPosX = m_GameBoard.getPosition().x + m_GameBoard.getGlobalBounds().width * 0.1f, 
+		l_fCardSlotPosY = m_GameBoard.getPosition().y + m_GameBoard.getGlobalBounds().width * 0.1f;
+
+	float l_fUpdatedPosX = l_fCardSlotPosX, l_fUpdatedPosY = l_fCardSlotPosY;
+
+	bool l_bFirstLoop = true; 
+
+	for (int i = 0; i < l_iNumberOfSlots; i++)
+	{
+		// Create Rectangle.
+
+		sf::RectangleShape l_TempRect; 
+
+		l_TempRect.setSize(sf::Vector2f(l_fCardSlotWidth, l_fCardSlotHeight));
+
+		l_TempRect.setPosition(sf::Vector2f(l_fUpdatedPosX, l_fUpdatedPosY));
+
+		l_TempRect.setFillColor(sf::Color::Cyan);
+
+		l_TempRect.setOutlineThickness(1.f); 
+		l_TempRect.setOutlineColor(sf::Color::Black)
+			;\
+		l_fUpdatedPosX += l_fCardSlotWidth * 1.5f;
+
+		// Add to Vector.
+
+		v_CardSlots.push_back(l_TempRect); 
+
+		// Update Position for next rectangle. 
+
+		if (i == 2 && l_bFirstLoop == true)
+		{
+			// New Row of cards. 
+
+			l_fUpdatedPosY += l_fCardSlotHeight * 2; 
+
+			l_fUpdatedPosX = l_fCardSlotPosX;
+
+			i = -1;
+
+			l_bFirstLoop = false; 
+		}
+	}
 
 }
 
